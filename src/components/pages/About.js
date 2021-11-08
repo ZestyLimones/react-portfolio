5e7844abc17f11dd2ccf774b4e90bcbe94607298
Micheal Limones <micheal@zestylimones.com>
2021-11-08 16:47:54 -0700

    Added content to About me page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,15 +4,28 @@ export default function About() {
   return (
     <div>
       <h2>About Me</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <img src="" alt="" />
+      <div class="card-content">
+        <p>
+          <span>Full Stack web developer</span> skilled in HTML5, CSS3,
+          JavaScript, jQuary, APIs, Node, and GitHub. Thrives on the challenge
+          of learning new technologies and the satisfaction of solving difficult
+          problems with <span>creative solutions</span>. I’m currently enrolled
+          in a Full Stack bootcamp through the University of Denver to expand my
+          skills and move into a Web Developer role.{' '}
+        </p>
+        <p>
+          Recently completed a group project working with two other individuals
+          to create a mental health application to track daily progress creating
+          intrinsic goals for our user. Our app sets up a loop reward system
+          creating daily patterns. The user checks in daily repeating tasks
+          proven to be beneficial to mental health increasing dopamine and
+          serotonin. ​Check out <span>Ouroboros</span> located within my
+          Projects section!The project utilized{' '}
+          <span>APIs, Handlebars, mySQL, Amazon Web Services </span> and a
+          significant amount of <span>JavaScript</span>.
+        </p>
+      </div>
     </div>
   );
 }
