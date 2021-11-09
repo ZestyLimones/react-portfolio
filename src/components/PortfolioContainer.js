445ca482735e142e0fc5c94b95a24fa2f874b99a
Micheal Limones <micheal@zestylimones.com>
2021-11-09 08:17:57 -0700

    added font and container styling.

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import NavBar from './NavBar';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Footer from './Footer';
 import Header from './Header';
+import './PortfolioContainer.css';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
