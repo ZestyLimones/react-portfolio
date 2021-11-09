456765d38ae0bee29c807bc03f621ed8cbe7afca
Micheal Limones <micheal@zestylimones.com>
2021-11-08 17:30:35 -0700

    added form to contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function Contact() {
+function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('')
+
   return (
     <div>
       <h2>Contact Me</h2>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <form className="contact-form">
+        <label>
+          Name:
+          <input type='text' name="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+        </label>
+        <label>
+          email:
+          <input type='email' name="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        </label>
+        <label>
+          message:
+          <textarea name="message" id="" cols="30" rows="10" value={message} onChange={(e) => setMessage(e.target.value)} required/>
+        </label>
+        <input type="submit" name="submit" id="" value='Submit' />
+      </form>
     </div>
   );
 }
+
+export default Contact;
