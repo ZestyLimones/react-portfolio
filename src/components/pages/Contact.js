b695df4942ac6957fcf77a7b9012c82e5c2806c7
Micheal Limones <micheal@zestylimones.com>
2021-11-11 12:12:52 -0700

    added sytling to contact form.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,27 +7,27 @@ function Contact() {
   const [message, setMessage] = useState('')
 
   return (
-    <div>
+    <div className="contact">
       <h2>Contact Me</h2>
       <div className="card">
         <div className="card-content">
             <p>A background in Benefits Administration and Human Resources gives me a <span>customer-focused</span> mentality and the ability to keep a project aligned with the needs of the end-user. I now apply this process to my work as a developer by <span>researching challenging issues </span>researching challenging issues that affect usability and accessibility and effectively collaborating with my team.</p>
-            <p>Feel free to contact me via email: <span><a id='email' href="mailto:micheal@zestylimones.com">micheal@zestylimones.com</a></span></p>
+            {/* <p>Feel free to contact me via email: <span><a id='email' href="mailto:micheal@zestylimones.com">micheal@zestylimones.com</a></span></p> */}
         </div>
         <form className="contact-form">
-          <label>
+          <label >
             Name:
-            <input type='text' name="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+            <input type='text' className="text-box" name="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+          </label>
+          <label >
+            Email:
+            <input type='email' className="text-box" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
           </label>
           <label>
-            email:
-            <input type='email' name="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            Message:
+            <textarea name="message" cols="100" rows="10" value={message} onChange={(e) => setMessage(e.target.value)} required/>
           </label>
-          <label>
-            message:
-            <textarea name="message" id="" cols="30" rows="10" value={message} onChange={(e) => setMessage(e.target.value)} required/>
-          </label>
-          <input type="submit" name="submit" id="" value='Submit' />
+          <input type="submit" name="submit"value='Submit' className="submit" />
         </form>
       </div>
       
