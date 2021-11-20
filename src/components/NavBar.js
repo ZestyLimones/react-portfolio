b74674ae75a6bafec7accf06044cc470333e7f73
Micheal Limones <micheal@zestylimones.com>
2021-11-20 08:53:31 -0700

    added breadcrumbs to nav bar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import "./NavBar.css"
+import Typography from '@mui/material/styles/createTypography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import './NavBar.css';
 
-function NavBar({ currentPage, handlePageChange}) {
-    return (
-        <div className="nav">
-            <ul className="nav-bar">
-                <li className="nav-item">
-                    <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === "About" ? 'nav-link active' : 'nav-link'}>About Me</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#projects" onClick={() => handlePageChange('Projects')} className={currentPage === "Projects" ? 'nav-link active' : 'nav-link'}>Projects</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === "Contact" ? 'nav-link active' : 'nav-link'}>Contact Me</a>
-                </li>
-                <li className="nav-item">
-                    <a href="https://drive.google.com/file/d/1RqQuLwDXdIFGJQak1mSKnHwn_4hwkzQn/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
-                </li>
-            </ul>
-        </div>
+// function handleClick(event) {
+//   event.preventDefault();
+//   console.info('You clicked a breadcrumb.');
+// }
 
-    )
+function NavBar() {
+  function handleClick(event) {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
+  }
+
+  return (
+    <div className="nav" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrump">
+        <Link href="#about">About Me</Link>
+        <Link href="#projects">Projects</Link>
+        <Link href="#contact">Contact Me</Link>
+      </Breadcrumbs>
+      {/* <ul className="nav-bar">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#projects"
+            onClick={() => handlePageChange('Projects')}
+            className={
+              currentPage === 'Projects' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Projects
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={
+              currentPage === 'Contact' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Contact Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="https://drive.google.com/file/d/1RqQuLwDXdIFGJQak1mSKnHwn_4hwkzQn/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
+        </li>
+      </ul> */}
+    </div>
+  );
 }
 
 export default NavBar;
