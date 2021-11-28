d453de974d6406aebcc52e7c602aa522527343dc
Micheal Limones <micheal@zestylimones.com>
2021-11-28 08:38:32 -0700

    added styling to navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import Typography from '@mui/material/styles/createTypography';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import './NavBar.css';
 
-// function handleClick(event) {
-//   event.preventDefault();
-//   console.info('You clicked a breadcrumb.');
-// }
-
 function NavBar({ currentPage, handlePageChange }) {
-  // function handleClick(event) {
-  //   event.preventDefault();
-  //   console.info('You clicked a breadcrumb.');
-  // }
-
   return (
-    <div className="nav">
-      <Breadcrumbs aria-label="breadcrump">
+    <div className="nav-bar">
+      <Box className="nav-item">
         <Link
+          underline="hover"
+          color="white"
           href="#about"
           onClick={() => handlePageChange('About')}
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
           About Me
         </Link>
+      </Box>
+      <Box className="nav-item">
         <Link
+          underline="hover"
+          color="white"
           href="#projects"
           onClick={() => handlePageChange('Projects')}
           className={
@@ -34,63 +29,29 @@ function NavBar({ currentPage, handlePageChange }) {
         >
           Projects
         </Link>
+      </Box>
+      <Box className="nav-item">
         <Link
+          underline="hover"
+          color="white"
           href="#contact"
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact Me
         </Link>
+      </Box>
+      <Box className="nav-item">
         <Link
+          underline="hover"
+          color="white"
           href="https://drive.google.com/file/d/1RqQuLwDXdIFGJQak1mSKnHwn_4hwkzQn/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
         >
           Resume
         </Link>
-      </Breadcrumbs>
-      {/* <ul className="nav-bar">
-        <li className="nav-item">
-          <a
-            href="#about"
-            onClick={() => handlePageChange('About')}
-            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-          >
-            About Me
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#projects"
-            onClick={() => handlePageChange('Projects')}
-            className={
-              currentPage === 'Projects' ? 'nav-link active' : 'nav-link'
-            }
-          >
-            Projects
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#contact"
-            onClick={() => handlePageChange('Contact')}
-            className={
-              currentPage === 'Contact' ? 'nav-link active' : 'nav-link'
-            }
-          >
-            Contact Me
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="https://drive.google.com/file/d/1RqQuLwDXdIFGJQak1mSKnHwn_4hwkzQn/view?usp=sharing"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Resume
-          </a>
-        </li>
-      </ul> */}
+      </Box>
     </div>
   );
 }
